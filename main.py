a0867828f66c81bcd9a3e04dcaa90f76a42395d0
Ray <seriouscoder123@gmail.com>
2024-02-20 20:00:47 +0800

    Use main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+
+from .bot.lumina import LuminaBot
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    TOKEN = os.getenv("DISCORD_TOKEN")
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.dm_messages = True
+
+    lumina = LuminaBot(command_prefix=commands.when_mentioned_or('/'), intents=intents)
+    
+    lumina.run(TOKEN)
