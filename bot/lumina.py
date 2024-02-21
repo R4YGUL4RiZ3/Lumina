600133a349a941f4621d38f415c2c0b12921b046
Ray <seriouscoder123@gmail.com>
2024-02-21 16:28:27 +0800

    Refactor (still some bugs to fix)

--- bot/lumina.py
+++ bot/lumina.py
@@ -1,23 +1,62 @@
 import os
+import sys
 import random
 import json
 from typing import *
 
 import discord
 from discord.ext import commands
-from openai import OpenAI
 
-from .chat import Chat
+from .client import Client
 from .utils import *
 
+class LuminaCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command()
+    async def roll(self, ctx: commands.Context):
+        if not self.bot.bot_active:
+            print("rolling...")
+            random_number = str(random.randint(1, 6))
+            await ctx.send(random_number)
+
+    @commands.command()
+    async def quit(self, ctx: commands.Context):
+        if self.bot.bot_active:
+            self.bot.bot_active = False
+            self.bot.client.cleanup()
+            await ctx.send("\n```You have successfully exit the conversation.```")
+
+    @commands.command()
+    async def join_voice(self, ctx: commands.Context):
+        channel = ctx.author.voice.channel
+        if channel:
+            await channel.connect()
+        else:
+            await ctx.send("You are not in a voice channel! Join one to get me to join.")
+        
+    @commands.command()
+    async def leave_voice(self, ctx: commands.Context):
+        voice = discord.utils.get(self.voice_clients, guild=self.guilds[0])
+        
+        if voice and voice.is_connected():
+            await voice.disconnect()
+            await ctx.channel.send("> Lumina Left the voice channel")
+        else:
+            await ctx.channel.send("I'm not in a voice channel!")
+    
 class LuminaBot(commands.Bot):
     def __init__(self, command_prefix, intents):
         super().__init__(command_prefix, intents=intents)
-        self.chat = Chat(self, load_instructions("./bot/instructions.txt"))
+        self.client = Client(load_instructions("./bot/instructions.txt"))
 
         self.bot_active = False
 
         self._cache_dir = "./cache"
+
+    async def setup_hook(self):
+        await self.add_cog(LuminaCog(self))
 
     async def on_ready(self):
         guild = self.guilds[0]
@@ -49,32 +88,28 @@ class LuminaBot(commands.Bot):
         if (self.user in message.mentions) or (message.guild is None):
             try:
                 self.bot_active = True
-                await self.chat.init_thread()
+                await self.client.init_thread()
                 await message.channel.send("```You have now entered a converation session with Lumina. Type `!quit` to quit.```\n")
             except Exception as e:
-                print(e)
+                print(e, file=sys.stderr)
                 await message.channel.send("```Something went wrong```")
 
         if self.bot_active:
             try:
                 async with message.channel.typing():
-                    await self.chat.add_message(message)
-                    await self.chat.respond(message)
+                    await self.client.add_message(f"<{message.author.display_name}> " + message.content)
+                    response = await self.client.respond()
+                    await message.channel.send(response)
+                    print(f"\nLUMINA: {response}")
             except Exception as e:
-                print(e)
+                print(e, file=sys.stderr)
                 await message.channel.send("```Something went wrong```")
 
-        await self._handle_commands(message)
         await self.process_commands(message)
 
-    async def _handle_commands(self, message):
-        if message.content.startswith('!'):
-            if not self.bot_active and message.content[1:] == "roll":
-                print("rolling...")
-                random_number = str(random.randint(1, 6))
-                await message.channel.send(random_number)
-
-            if self.bot_active and message.content[1:] == "quit":
-                self.bot_active = False
-                self.chat.cleanup()
-                await message.channel.send("\n```You have successfully exit the conversation.```")
+    async def _play(self):
+        voice_client = discord.utils.get(self.voice_clients, guild=self.guilds[0])
+        if not voice_client:
+            return
+        src = discord.FFmpegPCMAudio(os.path.join(self._cache_dir, "_current_audio.opus"))
+        voice_client.play(src, after=lambda e: print(f"Player error: {e}") if e else None)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import discord
 from discord.ext import commands
 
 from bot import LuminaBot
+# from bot.lumina import LuminaCog
 
 def main():
     load_dotenv()
@@ -14,6 +15,7 @@ def main():
     intents.dm_messages = True
 
     lumina = LuminaBot(command_prefix='!', intents=intents)
+    # lumina.add_cog(LuminaCog)
     
     lumina.run(TOKEN)
 
