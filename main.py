173431f083da21c156e1bbbf6190cf00eac8a694
Ray <seriouscoder123@gmail.com>
2024-02-21 16:30:12 +0800

    Remove commented lines

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import discord
 from discord.ext import commands
 
 from bot import LuminaBot
-# from bot.lumina import LuminaCog
 
 def main():
     load_dotenv()
@@ -15,7 +14,6 @@ def main():
     intents.dm_messages = True
 
     lumina = LuminaBot(command_prefix='!', intents=intents)
-    # lumina.add_cog(LuminaCog)
     
     lumina.run(TOKEN)
 
